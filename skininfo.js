f366fe8447c62ddca56b3772a5191f156a5f1544
litspeed <litspeed@nifty.com>
2017-02-02 15:07:38 +0900

    Update skininfo.js

--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'litspeedskin',
+        name: 'litspeed Skin'
     };
 });
