ee9935aca03c1516054ba678af3a37b65a001bf1
litspeed <litspeed@nifty.com>
2017-02-02 15:06:35 +0900

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'litspeed Skin';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'litspeedskin';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
